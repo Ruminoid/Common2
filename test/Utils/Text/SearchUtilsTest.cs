9b083d274401391839d79ccbd4ae9437aa4fc041
Il Harper <afanyiyu@hotmail.com>
2021-04-29 21:23:10 +0800

    feat: Add SearchUtils

---
+++ test/Utils/Text/SearchUtilsTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ruminoid.Common2.Utils.Text;
+using Xunit;
+
+namespace Ruminoid.Common2.Test.Utils.Text
+{
+    public static class SearchUtilsTest
+    {
+        [Fact]
+        public static void GetTextForSearchTextTest()
+        {
+            List<string> expected = new List<string>
+            {
+                // ReSharper disable StringLiteralTypo
+
+                "aafangshi",
+                "aafangs",
+                "aafshi",
+                "aafs"
+
+                // ReSharper restore StringLiteralTypo
+            };
+
+            List<string> actual = SearchUtils.GetTextForSearch("AA方式");
+
+            Assert.True(actual.All(expected.Contains));
+            Assert.Equal(expected.Count, actual.Count);
+        }
+
+        [Fact]
+        public static void SearchTest()
+        {
+            // ReSharper disable StringLiteralTypo
+
+            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafa").Any());
+            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafangs").Any());
+            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafangsh").Any());
+            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafsh").Any());
+            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "bb").Any());
+            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "aafangg").Any());
+            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "aafngshi").Any());
+
+            // ReSharper restore StringLiteralTypo
+        }
+    }
+}
