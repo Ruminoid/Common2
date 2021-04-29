2161214622b23b80e21dba8ce1e6eff494c1978a
Il Harper <afanyiyu@hotmail.com>
2021-04-29 21:22:59 +0800

    feat: Add TextUtils

---
+++ test/Utils/Text/TextUtilsTest.cs
@@ -0,0 +1,18 @@
+﻿using Ruminoid.Common2.Utils.Text;
+using Xunit;
+
+namespace Ruminoid.Common2.Test.Utils.Text
+{
+    public static class TextUtilsTest
+    {
+        [Fact]
+        public static void EngOrNumCharTest()
+        {
+            Assert.True("abc".IsEngOrNumChar());
+            Assert.True("123".IsEngOrNumChar());
+            Assert.True("123abc".IsEngOrNumChar());
+            Assert.False("123abc;".IsEngOrNumChar());
+            Assert.False("123abc我".IsEngOrNumChar());
+        }
+    }
+}
