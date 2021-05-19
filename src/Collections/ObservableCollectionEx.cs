28e4bb88b0ae1f4d3a0d0ec594d775dc4e477d0f
Il Harper <afanyiyu@hotmail.com>
2021-05-19 23:23:45 +0800

    feat: Add ObservableCollectionEx<T>

---
+++ src/Collections/ObservableCollectionEx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Ruminoid.Common2.Collections
+{
+    public class ObservableCollectionEx<T> : ObservableCollection<T>
+    {
+        public ObservableCollectionEx()
+        {
+        }
+
+        public ObservableCollectionEx(IEnumerable<T> collection)
+            : base(collection)
+        {
+        }
+
+        public ObservableCollectionEx(List<T> list)
+            : base(list)
+        {
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (T item in Items) action(item);
+        }
+
+        public void AddRange(IEnumerable<T> range)
+        {
+            foreach (var item in range) Items.Add(item);
+
+            OnPropertyChanged(new("Count"));
+            OnPropertyChanged(new("Item[]"));
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+        }
+
+        public void Reset(IEnumerable<T> range)
+        {
+            Items.Clear();
+
+            AddRange(range);
+        }
+    }
+}
