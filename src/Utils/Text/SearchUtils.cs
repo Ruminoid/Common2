1f64180f5a18011bc23666c087ea52754d1c289c
Il Harper <afanyiyu@hotmail.com>
2021-05-19 15:55:06 +0800

    feat: SearchUtils Update

--- src/Utils/Text/SearchUtils.cs
+++ src/Utils/Text/SearchUtils.cs
@@ -8,8 +8,125 @@ using Microsoft.International.Converters.PinYinConverter;
 namespace Ruminoid.Common2.Utils.Text
 {
     [PublicAPI]
+    public class SearchTrie<TValue>
+    {
+        public class Node
+        {
+            private HashSet<TValue> _word;
+
+            public HashSet<TValue> Word => _word ??= new();
+
+            public bool IsTerminal => Edges.Count == 0 && Word != null;
+
+            public Dictionary<string, Node> Edges = new();
+        }
+
+        public Node Root = new();
+
+        #region Utils
+
+        public static List<string> GenerateCharSearchPattern(char c)
+        {
+            List<string> result = new();
+
+            if (ChineseChar.IsValidChar(c))
+            {
+                // Chinese char
+
+                result.AddRange(
+                    new ChineseChar(c).Pinyins
+                        .Where(x => !string.IsNullOrWhiteSpace(x))
+                        .Select(x => Regex.Replace(x, @"\d", "").ToLower())
+                        .Distinct()
+                        .ToList()); // 全拼
+
+                result.AddRange(
+                    result
+                        .Select(x => x[..1]) // 首字母
+                        .ToArray());
+
+                result.Add(c.ToString()); // 原字符
+            }
+            else if (c.ToString().IsEngOrNumChar())
+            {
+                // Eng or num
+
+                result.Add(c.ToString().ToLower());
+                result.Add(c.ToString().ToUpper());
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        public SearchTrie(List<(string Key, TValue Value)> words)
+        {
+            for (int w = 0; w < words.Count; w++)
+            {
+                var word = words[w];
+                List<Node> nodes = new() {Root};
+                for (int len = 1; len <= word.Key.Length; len++)
+                {
+                    var letter = word.Key[len - 1];
+
+                    List<string> searchPattern = GenerateCharSearchPattern(letter);
+
+                    if (!searchPattern.Any()) continue;
+
+                    List<Node> nexts = new();
+
+                    foreach (Node node in nodes)
+                    {
+                        foreach (string s in searchPattern)
+                        {
+                            if (!node.Edges.ContainsKey(s))
+                                node.Edges[s] = new();
+
+                            nexts.Add(node.Edges[s]);
+                        }
+                    }
+
+                    //foreach (string s in searchPattern)
+                    //{
+                    //    if (node.Edges.ContainsKey(s))
+                    //        next = node.Edges[s];
+                    //}
+
+                    //foreach (string s in searchPattern)
+                    //    if (node.Edges.ContainsKey(s))
+                    //        next = node.Edges[s];
+
+                    //if (next is null) next = new();
+
+                    //foreach (string s in searchPattern)
+                    //{
+                    //    if (!node.Edges.TryGetValue(s, out next))
+                    //    {
+                    //        next = new Node();
+
+                    //        node.Edges.Add(s, next);
+                    //    }
+                    //}
+
+                    if (len == word.Key.Length)
+                    {
+                        foreach (Node next in nexts)
+                        {
+                            next.Word.Add(word.Value);
+                        }
+                    }
+
+                    nodes = nexts;
+                }
+            }
+        }
+    }
+
+    [PublicAPI]
     public static class SearchUtils
     {
+        [Obsolete]
         public static List<string> GetTextForSearch(string text)
         {
             List<List<string>> textList = new();
@@ -52,13 +169,15 @@ namespace Ruminoid.Common2.Utils.Text
                     current.DefaultIfEmpty()
                         .SelectMany(x =>
                             list.Select(y =>
-                                (x ?? "") + y))
+                                x + y))
                         .ToList());
         }
 
+        [Obsolete]
         public static List<string> Search(IEnumerable<string> items, string searchText) =>
             Search(items, x => x, searchText);
 
+        [Obsolete]
         public static List<T> Search<T>(IEnumerable<T> items, Func<T, string> text, string searchText)
         {
             searchText = searchText.ToLower();
@@ -67,6 +186,83 @@ namespace Ruminoid.Common2.Utils.Text
                 .Where(x => GetTextForSearch(text(x))
                     .Any(y => y.Contains(searchText)))
                 .ToList();
+        }
+
+        private static void CollectAllValues<T>(
+            SearchTrie<T>.Node node,
+            HashSet<T> result)
+        {
+            if (node.IsTerminal)
+            {
+                foreach (T w in node.Word)
+                {
+                    result.Add(w);
+                }
+            }
+
+            foreach (var edge in node.Edges)
+            {
+                CollectAllValues(edge.Value, result);
+            }
+        }
+
+        private static void SearchIntl<T>(
+            SearchTrie<T>.Node node,
+            string searchText,
+            HashSet<T> result)
+        {
+            foreach (var edge in node.Edges)
+            {
+                if (edge.Key.StartsWith(searchText) || searchText.StartsWith(edge.Key))
+                {
+                    if (edge.Value.IsTerminal)
+                    {
+                        foreach (T w in edge.Value.Word)
+                        {
+                            result.Add(w);
+                        }
+                    }
+
+                    if (edge.Key.Length <= searchText.Length)
+                        SearchIntl(edge.Value, searchText[edge.Key.Length..], result);
+                    else
+                        CollectAllValues(edge.Value, result);
+                }
+            }
+        }
+
+        private static void SearchIntlExt<T>(
+            SearchTrie<T>.Node node,
+            string searchText,
+            HashSet<T> result)
+        {
+            if (node.IsTerminal)
+                return;
+
+            foreach (var edge in node.Edges)
+            {
+                SearchIntl(
+                    edge.Value,
+                    searchText,
+                    result);
+                SearchIntlExt(
+                    edge.Value,
+                    searchText,
+                    result);
+            }
+        }
+
+        public static HashSet<T> Search<T>(
+            this SearchTrie<T> items,
+            string searchText)
+        {
+            searchText = searchText.ToLower();
+
+            HashSet<T> result = new();
+
+            SearchIntl(items.Root, searchText, result);
+            SearchIntlExt(items.Root, searchText, result);
+            return result;
         }
     }
 }
--- test/Utils/Text/SearchUtilsTest.cs
+++ test/Utils/Text/SearchUtilsTest.cs
@@ -1,57 +1,125 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Ruminoid.Common2.Utils.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Ruminoid.Common2.Test.Utils.Text
 {
-    public static class SearchUtilsTest
+    public class SearchUtilsTest
     {
+        #region Data
+
+        // ReSharper disable StringLiteralTypo
+
+        private static readonly List<string> Expected = new()
+        {
+            "aafangshi",
+            "aafangs",
+            "aafang式",
+            "aafshi",
+            "aafs",
+            "aaf式",
+            "aa方shi",
+            "aa方s",
+            "aa方式"
+            
+        };
+
+        private static readonly List<string> SearchDataA = new()
+        {
+            "aafa",
+            "aafangs",
+            "aafangsh",
+            "aafsh",
+            "afsh",
+            "fsh"
+        };
+
+        private static readonly List<string> SearchDataB = new()
+        {
+            "方sh",
+            "A方式"
+        };
+
+        private static readonly List<string> SearchDataC = new()
+        {
+            "bb",
+            "aafangg",
+            "aafngshi",
+            "啊",
+            "房十",
+            "a房s"
+        };
+
+        // ReSharper restore StringLiteralTypo
+
+        #endregion
+
         [Fact]
+        [Obsolete]
         public static void GetTextForSearchTextTest()
         {
-            List<string> expected = new()
-            {
-                // ReSharper disable StringLiteralTypo
-
-                "aafangshi",
-                "aafangs",
-                "aafang式",
-                "aafshi",
-                "aafs",
-                "aaf式",
-                "aa方shi",
-                "aa方s",
-                "aa方式"
-
-                // ReSharper restore StringLiteralTypo
-            };
-
             List<string> actual = SearchUtils.GetTextForSearch("AA方式");
-
-            Assert.True(actual.All(expected.Contains));
-            Assert.Equal(expected.Count, actual.Count);
+            Assert.True(actual.All(Expected.Contains));
+            Assert.Equal(Expected.Count, actual.Count);
         }
 
         [Fact]
-        public static void SearchTest()
+        [Obsolete]
+        public static void ObsoleteSearchTest()
         {
-            // ReSharper disable StringLiteralTypo
+            foreach (string s in SearchDataA) Assert.True(SearchUtils.Search(new[] {"AA方式"}, s).Any());
+            foreach (string s in SearchDataC) Assert.False(SearchUtils.Search(new[] { "AA方式" }, s).Any());
+        }
 
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafa").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafangs").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafangsh").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "aafsh").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "afsh").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "fsh").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "方sh").Any());
-            Assert.True(SearchUtils.Search(new[] { "AA方式" }, "A方式").Any());
-            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "bb").Any());
-            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "aafangg").Any());
-            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "aafngshi").Any());
-            Assert.False(SearchUtils.Search(new[] { "AA方式" }, "啊").Any());
+        private static bool SearchResultValidB(
+            List<string> expected,
+            HashSet<string> actual)
+        {
+            return (actual.All(expected.Contains)) &&
+                   expected.Count == actual.Count;
+        }
 
-            // ReSharper restore StringLiteralTypo
+        private static bool SearchResultValid(
+            HashSet<string> results,
+            string resultValue = "SEARCH_REAULT_A") =>
+            results.Count == 1 &&
+            results.First() == resultValue;
+
+        private readonly ITestOutputHelper _output;
+
+        public SearchUtilsTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void SearchTest()
+        {
+            SearchTrie<string> trie = new(new()
+            {
+                ("AA方式", "SEARCH_REAULT_A"),
+                ("AB模式", "SEARCH_REAULT_B"),
+                ("AA房十", "SEARCH_REAULT_C")
+            });
+
+            HashSet<string> result = trie.Search("aafshi");
+
+            // 拼音/首字母/同汉字搜索测试
+            foreach (string s in SearchDataA)
+            {
+                Assert.True(SearchResultValidB(new List<string>() {"SEARCH_REAULT_A", "SEARCH_REAULT_C"},
+                    trie.Search(s)));
+                _output.WriteLine(s + " passed.");
+            }
+            foreach (string s in SearchDataB) Assert.True(SearchResultValidB(new List<string>() { "SEARCH_REAULT_A" }, trie.Search(s)));
+            foreach (string s in SearchDataC) Assert.False(SearchResultValid(trie.Search(s)));
+
+            // 不同汉字搜索测试
+            Assert.False(SearchResultValid(trie.Search("AA房十")));
+            Assert.True(SearchResultValid(trie.Search("AA房十"), "SEARCH_REAULT_C"));
         }
     }
 }
