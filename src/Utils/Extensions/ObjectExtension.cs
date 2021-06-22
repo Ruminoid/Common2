bd7e2bf1c5ca975d6f1a7c49b739921fbd39d410
Il Harper <afanyiyu@hotmail.com>
2021-06-23 00:56:09 +0800

    feat: Add CloneUsingJson

---
+++ src/Utils/Extensions/ObjectExtension.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Ruminoid.Common2.Utils.Extensions
+{
+    public static class ObjectExtension
+    {
+        private static readonly JsonSerializerSettings SerializerSettings = new()
+        {
+            ObjectCreationHandling = ObjectCreationHandling.Replace
+        };
+
+        public static T CloneUsingJson<T>(this T source) =>
+            JsonConvert.DeserializeObject<T>(
+                JsonConvert.SerializeObject(source),
+                SerializerSettings);
+    }
+}
